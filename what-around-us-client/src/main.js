91cbfd594f5109408a10ba87efd84b0b9f881163
antoroute <54906304+antoroute@users.noreply.github.com>
2023-03-21 19:25:28 -0400

    install leaflet

--- what-around-us-client/src/main.js
+++ what-around-us-client/src/main.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'leaflet/dist/leaflet.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
